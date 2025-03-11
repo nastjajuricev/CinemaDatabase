e13031c94b323e5fd6858447e4f1157d3fcd45f7
nastjajuricev <92223715+nastjajuricev@users.noreply.github.com>
2025-03-11 12:01:24 +0000

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,65 @@
+// @ts-check
+
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config')
+} catch (e) {
+  // ignore error
+}
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    domains: ['dl.dropboxusercontent.com', 'm.media-amazon.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+    optimizeCss: true,
+    scrollRestoration: true,
+  },
+  swcMinify: true,
+}
+
+mergeConfig(nextConfig, userConfig)
+
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
+      }
+    } else {
+      nextConfig[key] = userConfig[key]
+    }
+  }
+}
+
+// Add bundle analyzer in production build
+const withBundleAnalyzer = process.env.ANALYZE === 'true'
+  ? (await import('@next/bundle-analyzer')).default({ enabled: true })
+  : (config) => config;
+
+export default withBundleAnalyzer(nextConfig);
+
